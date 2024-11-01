801106d16680fc3b2b69615f7a13e2b08f8416c3
Erwin van den Bogaard <evdbogaard@gmail.com>
2024-11-01 14:53:39 +0100

    Fix tiny issues

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 curl -s https://ohmyposh.dev/install.sh | bash -s
 export PATH=$PATH:/home/vscode/bin
 oh-my-posh font install meslo
-echo 'eval "$(oh-my-posh init bash --config /home/vscode/dotfiles/config.json)"' >> ~/.bashr
+echo 'eval "$(oh-my-posh init bash --config /home/vscode/dotfiles/omp.config.json)"' >> ~/.bashrc
 exec bash
